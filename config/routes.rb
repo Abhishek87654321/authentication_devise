4235e82216082ba06254c53773ac190306d06790
Abhishek <abhishekgangdhari@gmail.com>
2021-10-04 10:48:14 +0530

    another controller having index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root ‘application#index’
+  root 'user#index'
+  # root ‘application#index’
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
