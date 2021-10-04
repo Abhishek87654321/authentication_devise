58bffe4e8f9a591867b27cd4a627d03db10337d5
Abhishek <abhishekgangdhari@gmail.com>
2021-10-04 11:03:49 +0530

    fixed indentation problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
-	before_action :authenticate_user!
-	def index
-	  @user = current_user.email
-	end
+  before_action :authenticate_user!
+  def index
+    @user = current_user.email
+  end
 end
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class UserController < ApplicationController
   before_action :authenticate_user!
   def index
     @user = current_user.email
   end
-  end
+end
